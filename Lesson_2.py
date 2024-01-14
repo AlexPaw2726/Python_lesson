5d364e8ca8ac03b7e6f1004f7be97f15316169ed
Админ <AlexPaw2726@gmail.com>
2024-01-14 14:51:35 +0200

    second commit

---
+++ Lesson_2.py
@@ -0,0 +1,23 @@
+
+chislo_1 = 1234
+chislo_2 = 1234
+result_1 = chislo_1 // chislo_2
+print(result_1)
+
+
+chislo_3 = 1234
+chislo_4 = 617
+result_2 = chislo_3 // chislo_4
+print(result_2)
+
+
+chislo_5 = 1234
+chislo_6 = 411
+result_3 = chislo_5 // chislo_6
+print(result_3)
+
+
+chislo_7 = 1234
+chislo_8 = 308
+result_4 = chislo_7 // chislo_8
+print(result_4)
