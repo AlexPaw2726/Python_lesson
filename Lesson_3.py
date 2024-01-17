dcb47c9c8fe085197e55d8e7e02771bd3b712a7a
Админ <AlexPaw2726@gmail.com>
2024-01-17 23:09:06 +0200

    second commit

--- Lesson_3.py
+++ Lesson_3.py
@@ -8,10 +8,11 @@ elif question_2 == "-":
     print(int(question_1) - int(question_3))
 elif question_2 == "*":
     print(int(question_1) * int(question_3))
-elif question_2 == "//":
-     print(int(question_1) // int(question_3))
+elif question_2 == "//" and int(question_3) !=0:
+    print(int(question_1) // int(question_3))
+    print("delenie na nol")
+else:print("end")
 
-else: print("end")
 
 
 
