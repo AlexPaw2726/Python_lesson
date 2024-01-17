a00b57f6d48d782608c90fb47f560b4997c7ed45
Админ <AlexPaw2726@gmail.com>
2024-01-17 21:49:22 +0200

    first commit

--- Lesson_3.py
+++ Lesson_3.py
@@ -2,4 +2,14 @@ question_1 = input("write number :  ")
 question_2 = input("what action ?:  ")
 question_3 = input("write number :  ")
 
+if question_2 == "+":
+    print(int(question_1) + int(question_3))
+else:
+    print("end")
+
+
+
+
+
+
 
