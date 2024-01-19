d4d6d448ef119f39c1204b63b653c9bb0349883b
Админ <AlexPaw2726@gmail.com>
2024-01-19 14:37:54 +0200

    second commit

--- Lesson_3.py
+++ Lesson_3.py
@@ -1,27 +1,38 @@
-question_1 = int(input("write number :  "))
-question_2 = input("what action ?:  ")
-question_3 = int(input("write number :  "))
-
-if question_2 == "+":
-    result = (question_1 + question_3)
-    print(result)
-elif question_2 == "-":
-    result = (question_1 - question_3)
-    print(result)
-elif question_2 == "*":
-    result = (question_1 * question_3)
-    print(result)
-elif question_2 == "/":
-    if question_3 == 0:
-     print("error")
-else:
-    result = (question_1 / question_3)
-    print(result)
+# question_1 = int(input("write number :  "))
+# question_2 = input("what action ?:  ")
+# question_3 = int(input("write number :  "))
+#
+# if question_2 == "+":
+#     result = (question_1 + question_3)
+#     print(result)
+# elif question_2 == "-":
+#     result = (question_1 - question_3)
+#     print(result)
+# elif question_2 == "*":
+#     result = (question_1 * question_3)
+#     print(result)
+# elif question_2 == "/":
+#     if question_3 == 0:
+#      print("error")
+# else:
+#     result = (question_1 / question_3)
+#     print(result)
 
 value_list = [12, 3, 4, 10]
-value_list[0] = 10
-value_list[3] = 12
+print(len(value_list))
+num = 10
+value_list[-1] = num
+value_list.pop(-1)
+value_list.insert(0,num)
 print(value_list)
+
+
+
+
+
+
+
+
 
 
 
