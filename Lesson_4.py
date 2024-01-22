8714c82b704ad575423de23c960fd38e1f262197
Админ <AlexPaw2726@gmail.com>
2024-01-22 15:27:40 +0200

    first commit

--- Lesson_4.py
+++ Lesson_4.py
@@ -1,7 +1,20 @@
-value_list = [1,2,8,6,5]
-print(value_list)
-value_list.reverse()
-print(value_list)
+# value_list = [1,2,8,6,5]
+# print(value_list)
+# value_list.reverse()
+# print(value_list)
+#
+# value_list.sort()
+# print(value_list)
 
-value_list.sort()
-print(value_list)
+# value_int = 0
+# is_true = True
+#
+# while is_true:
+#     value_int += 1
+#     print(value_int)
+#     if value_int > 3:
+#       break
+
+my_list = [0, 1, 0, 12, 3]
+my_list.sort(reverse=True, key=bool)
+print(my_list)
