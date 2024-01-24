405f17496a525da14ee1fcc72a3a7d43bdb97b8b
Админ <AlexPaw2726@gmail.com>
2024-01-24 20:54:26 +0200

    first commit

--- Lesson_5.py
+++ Lesson_5.py
@@ -1,2 +1,47 @@
 
 
+# value = "_"
+# value_is_digit = value.isascii()
+# print(value_is_digit)
+
+# value = "x"
+# value_is_digit = value.isalpha()
+# print(value_is_digit)
+
+# value = "get_value"
+# value_is_digit = value.islower()
+# print(value_is_digit)
+
+
+# value = "get value"
+# value_is_digit = value.isdecimal()
+# print(value_is_digit)
+
+# value = "get!value"
+# value_is_digit = value.isnumeric()
+# print(value_is_digit)
+
+# value = "some_super_puper_value"
+# value_is_digit = value.islower()
+# print(value_is_digit)
+
+# value = "Get_value"
+# value_is_digit = value.islower()
+# print(value_is_digit)
+
+# value = "get_Value"
+# value_is_digit = value.isdecimal()
+# print(value_is_digit)
+
+# value = "getValu"
+# value_is_digit = value.isnumeric()
+# print(value_is_digit)
+
+# value = "3m"
+# value_is_digit = value.isnumeric()
+# print(value_is_digit)
+
+# value = "m3"
+# value_is_digit = value.isprintable()
+# print(value_is_digit)
+
