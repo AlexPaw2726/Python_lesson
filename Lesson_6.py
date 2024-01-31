f05867e05bb4eb77b988488e6f3d425b71748021
Админ <AlexPaw2726@gmail.com>
2024-01-31 22:24:53 +0200

    first commit

---
+++ Lesson_6.py
@@ -0,0 +1,18 @@
+value_dict_1 = {"name": "John", "age": 15}
+value_dict_2 = {"name": "Jack", "age" : 45}
+value_dict_3 = {"name": "Alex", "age" : 38}
+
+for i in value_dict_1,value_dict_2,value_dict_3:
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
