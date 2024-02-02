67598761d2dfb86209893f4c139d25b341df81d8
Админ <AlexPaw2726@gmail.com>
2024-02-02 20:56:52 +0200

    first commit

--- Lesson_6.py
+++ Lesson_6.py
@@ -3,8 +3,37 @@ value_dict = [{"name": "John", "age": 15},
               {"name": "Alex", "age": 38},
               {"name": "Michal", "age": 20},
               {"name": "Viktoriya", "age": 61}]
-for letter in value_dict:
-    print(letter)
+young_list = []
+long_name_list = []
+min_age = 1000
+max_name_len = 0
+av_age = 0
+
+for person in value_dict:
+    age = person.get("age")
+    name_len = len(person.get("name"))
+    av_age += age
+    if age == min_age:
+        young_list.append(person.get("name"))
+    elif age < min_age:
+        young_list.clear()
+        young_list.append(person.get("name"))
+        min_age = age
+    if name_len == max_name_len:
+        long_name_list.append(person.get("name"))
+    elif name_len > max_name_len:
+        long_name_list.clear()
+        long_name_list.append(person.get("name"))
+        max_name_len = name_len
+av_age = round(av_age/len(value_dict))
+print(young_list)
+print(long_name_list)
+print(av_age)
+
+
+
+
+
 
 
 
